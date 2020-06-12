d7a4d77546baec3cec46a59a716a40211d1df498
Henrique Luiz <hl.silva89@gmail.com>
2020-06-12 11:28:35 -0300

    test memory

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest2 as unittest
+
+from __future__ import print_function
+import psutil
+print(psutil.cpu_percent())
+print(psutil.virtual_memory())  # physical memory usage
+print('memory % used:', psutil.virtual_memory()[2])
+
+class TestMemory()
